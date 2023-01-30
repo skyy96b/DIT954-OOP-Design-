e37631de9ca7de132b802cfa5a1654d54095e926
skyy96b <123081707+skyy96b@users.noreply.github.com>
2023-01-30 18:27:00 +0100

    Add files via upload

---
+++ src/main/java/Transportable.java
@@ -0,0 +1,8 @@
+/**
+ * Contract for being transportable (as in contrast to being Loadable, i.e can load transportables)
+ */
+public interface Transportable {
+
+    public void setTransporter(IVehicle transporter);
+
+}
